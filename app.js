d65f66bcaa898c16b1ada4a4a1b1729af868e836
olxzr <olxazar96@mail.ru>
2024-06-17 22:40:03 +0300

    TS урок 1

---
+++ app.js
@@ -0,0 +1,11 @@
+let bool = true;
+bool = true;
+let num = 10;
+num = 15;
+let str = 'hello';
+str = 'hello world';
+let nul = undefined;
+// ошибки нет потому что тип void не возвращает нечего 
+let func = () => {
+    console.log('hello');
+};
---
+++ app.ts
@@ -0,0 +1,16 @@
+let bool: boolean = true
+bool = true
+
+let num: number = 10
+num = 15
+
+let str: string = 'hello'
+str = 'hello world'
+
+let nul: undefined = undefined
+
+// ошибки нет потому что тип void не возвращает нечего 
+let func = (): void => {
+    console.log('hello');
+}
+
